e71313fcbef274bb7a139b040aa5442badf468c9
Seoyun0626 <tjdbs0626@gmail.com>
2024-10-28 17:22:37 +0900

    Update 백준_7576_토마토.py
    시간 초과 해결(queue값에 좌표랑 날짜 함께 추가)

--- DFS_BFS/백준_7576_토마토.py
+++ DFS_BFS/백준_7576_토마토.py
@@ -3,58 +3,33 @@ from collections import deque
 import sys
 input = sys. stdin.readline
 sys.setrecursionlimit(10**5)
+check1 = []
 move = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-cnt = 0
 
 n, m = map(int, input().split())
 info = []
+queue = deque()
 for _ in range(m):
     info.append(list(map(int, input().split())))
-visited = [[False for _ in range(n)] for _ in range(m)]
-# print(visited)
+max_day = 0
 
-def bfs(info1):
-    global cnt
-    queue = deque()
-    # print("enter bfs", info1)
+for y in range(m):
+    for x in range(n):
+        if info[y][x] == 1:
+            queue.append((y, x, 0)) # 좌표 + 날짜 추가
 
-    for tmp_info1 in info1:
-        # print(tmp_info1)
-        queue.append(tmp_info1)
-        y, x = tmp_info1
-        visited[y][x] = True
-        # print("now queue", queue)
+while queue:
+    y, x, day = queue.popleft()
+    # print("y, x, day", y, x, day)
+    if max_day < day:
+        max_day = day
+    for dy, dx in move:
+        if 0 <= y + dy < m and 0 <= x + dx < n and info[y + dy][x + dx] == 0:
+            queue.append((y + dy, x + dx, day + 1))
+            info[y + dy][x + dx] = 1
 
-    while queue:
-        y, x = queue.popleft()
-        for dy, dx in move:
-            if 0 <= y + dy < m and 0 <= x + dx < n and not visited[y + dy][x + dx] and info[y + dy][x + dx] == 0:
-                info[y + dy][x + dx] = 1
-
-
-## '1'좌표 저장 함수
-def check1():
-    tmp_check1 = []
-    for y in range(m):
-        for x in range(n):
-            if info[y][x] == 1 and not visited[y][x]:
-                tmp_check1.append([y, x])
-    return tmp_check1
-
-## 종료 체크 함수
-def isFinished():
-    for y in range(m):
-        for x in range(n):
-           if info[y][x] == 0:
-               return False
-    return True
-
-while not isFinished():
-    result = check1()
-    if len(result) == 0:
-        print("-1")
+for row in info:
+    if 0 in row:
+        print(-1)
         exit(0)
-    else:
-        cnt += 1
-        bfs(result)
-print(cnt)
+print(max_day)

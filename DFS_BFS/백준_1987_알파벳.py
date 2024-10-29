56cef65c85fbe0b120cfcf9864c2f69241164337
Seoyun0626 <tjdbs0626@gmail.com>
2024-10-29 19:04:08 +0900

    Update 백준_1987_알파벳.py
    이미 범위를 알고 있는 경우 : 배열 세팅 후 index확인하는 방식 -> 시간 초과 해결

--- DFS_BFS/백준_1987_알파벳.py
+++ DFS_BFS/백준_1987_알파벳.py
@@ -1,26 +1,28 @@
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10**6)
 info = []
-alpha = [False] * 26
 move = [[-1, 0], [1, 0], [0, -1], [0, 1]]
-max_cnt = 1
+cnt = 1
 r, c = map(int, input().split())
 for _ in range(r):
     info.append(list(input().rstrip()))
-def dfs(y, x, cnt):
+check = [0] * 26
+check[ord(info[0][0]) - ord('A')] = 1
+def dfs(y, x, dist, check):
     # print("enter dfs", y, x, cnt)
-    global max_cnt
+    global cnt
+    cnt = dist
     # print("가능한 좌표", check, visited, y, x)
-    max_cnt = max(max_cnt, cnt)
     for i in range(4):
         nx, ny = x + move[i][0], y + move[i][1]
-
         if 0 <= ny < r and 0 <= nx < c:
-            if not alpha[info[ny][nx]]:
-                alpha[info[ny][nx]] = True
-                dfs(ny, nx, cnt + 1)
-                alpha[info[ny][nx]] = False
-alpha[info[0][0]] = True
-dfs(0, 0, 1)
-print(max_cnt)
+            tmp = ord(info[ny][nx]) - ord('A')
+            if not check[tmp]:
+                check[tmp] = 1
+                cnt = max(cnt, dfs(ny, nx, dist + 1, check))
+                check[tmp] = 0
+    return cnt
+
+
+result = dfs(0, 0, 1, check)
+print(result)
